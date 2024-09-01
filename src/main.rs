4e1f89d155488f2a376afa1218607c240c62b0c0
Afsal Thaj <afsal.taj06@gmail.com>
2024-09-01 22:51:28 +1000

    Make post

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
-use reqwest::header::{ACCEPT, AUTHORIZATION};
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    let url = "https://ribb.api.golem.cloud/v8/connect/afsal";
-    let token = "mytoken";
+    let url = "https://x.api.golem.cloud/api/v3/user";
 
-    // Create a client
     let client = reqwest::Client::new();
 
-    // Send GET request
+    // JSON body
+    let json_body = json!({
+        "id": "afsalthaj",
+        "name": "AfsalThaj",
+        "email": "adam@golem.cloud"
+    });
+
     let response = client
         .post(url)
         .header(ACCEPT, "application/json")
-        .header(AUTHORIZATION, format!("Bearer {}", token))
+        .json(&json_body)
         .send()
-        .await.unwrap();
+        .await;
 
-    // Check if the response status is successful
-    if response.status().is_success() {
-        // Parse the response body as JSON (or handle it as needed)
-        let json: serde_json::Value = response.json().await.unwrap();
-        println!("Response JSON: {:?}", json);
-    } else {
-        println!("Request failed with status: {}", response.status());
+    match response {
+        Ok(response) => {
+            if response.status().is_success() {
+                let id_out_result = response.text().await;
+                match id_out_result {
+                    Ok(id_out) => {
+                        println!("Response ID: {}", id_out);
+                    }
+                    Err(e) => {
+                        println!("Failed to read response ID: {}", e);
+                    }
+                }
+            } else {
+                println!("Request failed with status: {}", response.status());
+            }
+        }
+        Err(e) => {
+            println!("Request failed with error: {}", e);
+        }
     }
 }
